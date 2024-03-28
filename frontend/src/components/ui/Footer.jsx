ceac83397e513bf3863b856df494ff09ffc057c2
Arbaz Khan <arbazkhangul123@gmail.com>
2024-03-28 10:50:13 +0500

    deploy

---
+++ frontend/src/components/ui/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='bg-white mt-[1.4rem] text-[#16a085] text-center text-[1.3rem] py-[0.5rem] px-[1rem]'>Created By Arbaz Khan</div>
+  )
+}
+
+export default Footer
